ac235209aae8db02a371705b27f4472f8717c29d
Thiago Martines <142542192+TGMartines@users.noreply.github.com>
2023-10-09 17:29:57 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaDeCadastroDeContatos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
