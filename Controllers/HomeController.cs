d3f0c0adff9c4ca1ca0cb47b262631409c93ac3e
Thiago Martines <142542192+TGMartines@users.noreply.github.com>
2023-10-10 17:19:53 -0400

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,13 +6,7 @@ namespace SistemaDeCadastroDeContatos.Controllers;
 
 public class HomeController : Controller
 {
-
     public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
     {
         return View();
     }
